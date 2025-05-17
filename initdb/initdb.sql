b1ce4e4803ca9f1a86594f5d29fd15fd416b34b7
Andrey <hutornoyaa@gmail.com>
2025-05-17 18:07:31 +0000

    add mail to docker

--- initdb/initdb.sql
+++ initdb/initdb.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE homerep_auth;
 CREATE DATABASE homerep_userservice;
 CREATE DATABASE homerep_orders;
+CREATE DATABASE homerep_notif;
 ALTER SYSTEM SET max_connections = 1000;
 SELECT COUNT(*) from pg_stat_activity;
